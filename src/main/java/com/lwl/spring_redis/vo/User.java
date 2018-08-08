a1982680e07db2d073ba7b85b90da7ad0effab8d
WorsonRay <1132321713@qq.com>
2018-08-08 09:36:13 +0800

    上传springmvc_redis项目

---
+++ src/main/java/com/lwl/spring_redis/vo/User.java
@@ -0,0 +1,51 @@
+package com.lwl.spring_redis.vo;
+
+import java.io.Serializable;
+
+public class User implements Serializable{
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -5244288298702801619L;
+    private int id;
+    private String userName;
+    private String sex;
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getsex() {
+        return sex;
+    }
+
+    public void setsex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", userName=" + userName + ", sex=" + sex + ", age=" + age + "]";
+    }
+}
